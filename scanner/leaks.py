b5a9d457da5a4e1d46f83afd4b153f7cb2a11bb3
Elie Chahwan <eliechah@proton.me>
2025-05-18 22:47:46 +1000

    Tidied up file names and content, Finalised README.md

--- scanner/leaks.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-def run_unsafe():
-    user_input = input("Enter shell command: ")
-    subprocess.call(user_input, shell=True)  # command injection
-
-def hardcoded_secret():
-    password = "supersecret"  # hardcoded credential
