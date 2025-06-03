6bf0ff28a84929f0862f1c57279a82b43f1fbaaf
Elie Chahwan <eliechah@proton.me>
2025-06-03 23:30:53 +1000

    Cleaned up ai_filter.py

--- ai-model/ai_filter.py
+++ ai-model/ai_filter.py
@@ -1,12 +1,11 @@
 import os
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"     # Suppress TF INFO & WARN
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"    # Disable GPU / cuDNN logs
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
-import tensorflow as tf # type: ignore
-from tensorflow.keras.preprocessing.sequence import pad_sequences # type: ignore
+import tensorflow as tf
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 import pickle
 
-# Load model and tokenizer once
 model = tf.keras.models.load_model("kdnn_model.keras")
 with open("tokenizer.pkl", "rb") as f:
     tokenizer = pickle.load(f)
